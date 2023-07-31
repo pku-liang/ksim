5028d8dbe5c454f3e2abbbb14b25748e50bb02de
KEKE046 <KEKE_046@pku.edu.cn>
2023-07-31 16:40:03 +0800

    update setup script

--- third_party/setup-circt.sh
+++ third_party/setup-circt.sh
@@ -12,25 +12,22 @@ pushd llvm
 [ -d build ] || mkdir build
 cd build
 cmake ../llvm \
-  -GNinja \
   -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_ENABLE_ASSERTIONS=ON \
   -DLLVM_ENABLE_PROJECTS="mlir" \
   -DLLVM_TARGETS_TO_BUILD="X86;RISCV" \
-
-ninja install
+make install
 
 popd
 
 [ -d build ] || mkdir build
 cd build
 cmake .. \
-  -GNinja \
   -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_ENABLE_ASSERTIONS=ON \
   -DMLIR_DIR=$PWD/../llvm/build/lib/cmake/mlir \
   -DLLVM_DIR=$PWD/../llvm/build/lib/cmake/llvm \
   -DESI_COSIM=OFF -DESI_CAPN=OFF
-ninja install
+make install
