9c72bc229c9b396050cefe5e8fc54cff431e66c1
KEKE046 <KEKE_046@pku.edu.cn>
2024-04-11 07:09:41 +0000

    update test script

--- auto-test/config.mk
+++ auto-test/config.mk
@@ -1,14 +1,21 @@
 cases := \
-	GemminiMesh32x32 \
 	SmallBoomCore \
+	MediumBoomCore \
+	LargeBoomCore \
+	MegaBoomCore \
 	GemminiMeshFP6x6 \
 	GemminiMeshFP8x8 \
+	GemminiMeshFP10x10 \
+	GemminiMeshFP12x12 \
 	GemminiMesh6x6 \
 	GemminiMesh8x8 \
 	GemminiMesh10x10 \
 	GemminiMesh12x12 \
 	GemminiMesh16x16 \
-	GemminiMesh32x32 \
+	GemminiMesh32x32
 
 cases_dir := $(KSIM_ROOT)/auto-test/cases
-runs := ksim-1  ksim-12  ksim-16  ksim-2  ksim-20  ksim-24  ksim-28  ksim-3  ksim-32  ksim-4  ksim-5  ksim-6  ksim-7  ksim-8
+runs := ksim-1 ksim-2 ksim-3 ksim-4 ksim-5 ksim-6 ksim-7 ksim-8 ksim-9 ksim-10 \
+		ksim-11 ksim-12 ksim-13 ksim-14 ksim-15 ksim-16 ksim-17 ksim-18 ksim-19 ksim-20 \
+		ksim-21 ksim-22 ksim-23 ksim-24 ksim-25 ksim-26 ksim-27 ksim-28 ksim-29 ksim-30 \
+		ksim-31 ksim-32
--- auto-test/run.sh
+++ auto-test/run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 start_cpu=16
-rg8() {
-    seq $1 $(($1+7))
-}
-threads=($(rg8 1) $(rg8 129) $(rg8 9) $(rg8 137))
+threads=($(seq 1 32))
 nthread() {
     res=$(printf %s, "${threads[@]:0:$1}")
     echo ${res%%,}
@@ -15,7 +12,7 @@ for f in ./runs/*/; do
     for exe in $f/bin/*.exe; do
         exe_name=${exe##*/}
         exe_name=${exe_name%%.exe}
-        for ((i=0;i<50;i++)); do
+        for ((i=0;i<10;i++)); do
             echo -n $th,$i,$exe_name,
             taskset -c $(nthread $th) $exe 10000
         done
